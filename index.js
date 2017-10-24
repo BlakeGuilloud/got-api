34ee8a4fe6050f5ab2cce956606ad31b4f1eda6f
Blake Guilloud <blake.guilloud@gmail.com>
2017-10-24 16:12:11 -0400

    ...

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const serverless = require('serverless-http');
 
 const app = express();
 
 const schema = require('./schema');
 
 app.use(cors());
+app.use(bodyParser.json({ strict: false }));
 
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true,
 }));
 
-app.listen(4000);
-
+// app.listen(4000);
+module.exports.handler = serverless(app);
 console.log('listening');
